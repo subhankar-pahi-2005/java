f426e60304bb2d2c8dc4ab0d847e283416bdf67b
subhankar pahi <110842641+subhankar-pahi-2005@users.noreply.github.com>
2022-10-23 11:40:46 +0530

    Add files via upload

---
+++ Expt_17.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Expt_17 {
+    public static void main(String[] args) {
+        Scanner myObj=new Scanner(System.in);
+        String userName;
+        System.out.println("enter your name");
+        userName=myObj.nextLine();
+        System.out.println("your name is :"+userName);
+    }
+}
