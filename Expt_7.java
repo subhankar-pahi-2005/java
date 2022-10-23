965389b1c15752624d47ab222fc4f3234c73520f
subhankar pahi <110842641+subhankar-pahi-2005@users.noreply.github.com>
2022-10-23 11:37:34 +0530

    Add files via upload

---
+++ Expt_7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Expt_7 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int number1;
+        int number2;
+        System.out.println("input first integer: ");
+        number1=input.nextInt();
+        System.out.println("input second integer: ");
+        number2=input.nextInt();
+        if( number1==number2)
+            System.out.printf("%d==%d\n",number1,number2);
+        if(number1 !=number2)
+            System.out.printf("%d !=%d\n",number1,number2);
+        if(number1 < number2)
+            System.out.printf("%d <%d\n",number1,number2);
+        if(number1>number2)
+            System.out.printf("%d>%d\n",number1,number2);
+        if(number1<=number2)
+            System.out.printf("%d<=%d\n",number1,number2);
+        if(number1>=number2)
+            System.out.printf("%d>=%d",number1,number2);
+    }
+}
