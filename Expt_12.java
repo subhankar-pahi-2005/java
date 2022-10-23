c7646d38e76cd364a665d656b2b22b0133ada303
subhankar pahi <110842641+subhankar-pahi-2005@users.noreply.github.com>
2022-10-23 11:40:04 +0530

    Add files via upload

---
+++ Expt_12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Expt_12 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("input the first number: ");
+        int a=in.nextInt();
+        System.out.println("input second number: ");
+        int b= in.nextInt();
+        System.out.println("Result:"+result(a,b));
+    }
+    public static int result(int x,int y)
+    {
+        if (x==y)
+            return 0;
+        if (x %6==y%6)
+            return(x<y)?x:y;
+        return(x>y)?x:y;
+    }
+}
